1b570648de5ca7a68f634d4945552b77fe9fb70d
umaruch <130343724+starponycorp@users.noreply.github.com>
2023-06-01 16:15:20 +0500

    Update TitleRepository.java

--- src/main/java/com/starpony/prohojemba/titles/TitleRepository.java
+++ src/main/java/com/starpony/prohojemba/titles/TitleRepository.java
@@ -1,4 +1,15 @@
 package com.starpony.prohojemba.titles;
 
+import java.util.List;
+
 public interface TitleRepository {
+    public List<Title> findAll(QueryParams queryParams);
+
+    public Title find(int id);
+
+    public void create(Title title);
+
+    public void update(Title title);
+
+    public void delete(Title title);
 }
