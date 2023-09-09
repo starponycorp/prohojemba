a1999a5d2a21944719824f4994db87cf4b7dff64
umaruch <130343724+starponycorp@users.noreply.github.com>
2023-09-09 19:15:48 +0500

    add tests

--- src/main/java/com/starpony/prohojemba/repositories/RefreshTokenRedisRepository.java
+++ src/main/java/com/starpony/prohojemba/repositories/RefreshTokenRedisRepository.java
@@ -1,10 +1,15 @@
 package com.starpony.prohojemba.repositories;
 
+import com.starpony.prohojemba.exceptions.ServerException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
 import redis.clients.jedis.JedisPooled;
+import redis.clients.jedis.exceptions.JedisException;
 import redis.clients.jedis.params.SetParams;
 
+
+@Repository
 public class RefreshTokenRedisRepository implements RefreshTokenRepository{
     private final JedisPooled jedis;
     private final SetParams params;
@@ -19,17 +24,22 @@ public class RefreshTokenRedisRepository implements RefreshTokenRepository{
 
     @Override
     public void create(String refreshToken, int accountId) {
-        jedis.set(refreshToken, String.valueOf(accountId), params);
+        String resp = jedis.set(refreshToken, String.valueOf(accountId), params);
+        if (!resp.equals("OK"))
+            throw new ServerException(String.format("Save refresh token error. Redis response: %s", resp));
     }
 
     @Override
     public int get(String refreshToken) {
-        return Integer.parseInt(jedis.get(refreshToken));
+        try {
+            return Integer.parseInt(jedis.get(refreshToken));
+        } catch (NumberFormatException ex) {
+            return 0;
+        }
     }
 
     @Override
     public void delete(String refreshToken) {
         jedis.del(refreshToken);
-        System.out.printf("1");
     }
 }
