d29e05dfc0f7d110d2db97b55aa82300245d2abb
umaruch <130343724+starponycorp@users.noreply.github.com>
2023-09-11 21:23:26 +0500

    add UserDetailsService Bean

--- src/main/java/com/starpony/prohojemba/ApplicationConfig.java
+++ src/main/java/com/starpony/prohojemba/ApplicationConfig.java
@@ -2,12 +2,15 @@ package com.starpony.prohojemba;
 
 import com.starpony.prohojemba.security.JWTAuthProvider;
 import com.starpony.prohojemba.security.JWTFilter;
+import com.starpony.prohojemba.utils.JWTUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import redis.clients.jedis.JedisPooled;
@@ -22,6 +25,12 @@ public class ApplicationConfig {
     @Bean
     public JedisPooled jedisPooledConnections(@Value("${redis.host}") String host, @Value("${redis.port}") int port) {
         return new JedisPooled(host, port);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(JWTUtils jwtUtils) {
+        return token -> jwtUtils.extractAccessToken(token).orElseThrow(() ->
+                new UsernameNotFoundException("Invalid access token"));
     }
 
     /*
