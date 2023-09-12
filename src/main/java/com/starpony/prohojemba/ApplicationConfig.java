03733bbf5fef37707799c724542dfb1d83f8bee4
umaruch <130343724+starponycorp@users.noreply.github.com>
2023-09-12 10:56:47 +0500

    Update ApplicationConfig.java

--- src/main/java/com/starpony/prohojemba/ApplicationConfig.java
+++ src/main/java/com/starpony/prohojemba/ApplicationConfig.java
@@ -6,11 +6,17 @@ import com.starpony.prohojemba.utils.JWTUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import redis.clients.jedis.JedisPooled;
@@ -28,9 +34,27 @@ public class ApplicationConfig {
     }
 
     @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     public UserDetailsService userDetailsService(JWTUtils jwtUtils) {
         return token -> jwtUtils.extractAccessToken(token).orElseThrow(() ->
                 new UsernameNotFoundException("Invalid access token"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(JWTUtils jwtUtils) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService(jwtUtils));
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return  configuration.getAuthenticationManager();
     }
 
     /*
