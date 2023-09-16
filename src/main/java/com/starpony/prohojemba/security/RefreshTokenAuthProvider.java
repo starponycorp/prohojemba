868df55cb055b85cb269a765e84c6fddb5aabbc0
umaruch <130343724+starponycorp@users.noreply.github.com>
2023-09-17 00:17:41 +0500

    Update RefreshTokenAuthProvider.java

--- src/main/java/com/starpony/prohojemba/security/RefreshTokenAuthProvider.java
+++ src/main/java/com/starpony/prohojemba/security/RefreshTokenAuthProvider.java
@@ -1,32 +1,64 @@
 package com.starpony.prohojemba.security;
 
+import com.starpony.prohojemba.models.Account;
+import com.starpony.prohojemba.repositories.AccountsDatabaseRepository;
+import com.starpony.prohojemba.repositories.RefreshTokenRepository;
+import com.starpony.prohojemba.utils.JWTUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
+
+import javax.security.auth.login.AccountExpiredException;
 
 
 @Component
-public class RefreshTokenAuthProvider extends AbstractUserDetailsAuthenticationProvider {
+public class RefreshTokenAuthProvider implements AuthenticationProvider {
+    private final AccountsDatabaseRepository accountsRepository;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final JWTUtils jwtUtils;
+
+    @Autowired
+    public RefreshTokenAuthProvider(AccountsDatabaseRepository accountsRepository, RefreshTokenRepository refreshTokenRepository, JWTUtils jwtUtils) {
+        this.accountsRepository = accountsRepository;
+        this.refreshTokenRepository = refreshTokenRepository;
+        this.jwtUtils = jwtUtils;
+    }
+
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        return super.authenticate(authentication);
+        String refreshToken = authentication.getCredentials().toString();
+
+        Account account = jwtUtils.extractRefreshToken(refreshToken).orElseThrow(() ->
+                new BadCredentialsException("Invalid refresh token"));
+
+        int accountId = refreshTokenRepository.get(refreshToken);
+        if (accountId != account.getId())
+            throw new BadCredentialsException("Invalid refresh token");
+
+        account = accountsRepository.getById(accountId).orElseThrow(() ->
+               new UsernameNotFoundException(String.format("Account with id=%s not found", accountId)));
+
+        if (account.getIsLocked()) {
+            throw new DisabledException("Account is locked");
+        }
+
+        RefreshTokenAuthentication newAuthentication = new RefreshTokenAuthentication(
+                null, null, account, account.getAuthorities());
+        newAuthentication.setAuthenticated(true);
+
+        return newAuthentication;
     }
 
     @Override
     public boolean supports(Class<?> authentication) {
-        return super.supports(authentication);
-    }
-
-    @Override
-    protected void additionalAuthenticationChecks(UserDetails userDetails, UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
-
-    }
-
-    @Override
-    protected UserDetails retrieveUser(String username, UsernamePasswordAuthenticationToken authentication) throws AuthenticationException {
-        return null;
+        return authentication.equals(RefreshTokenAuthentication.class);
     }
 }
