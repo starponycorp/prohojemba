e85e030e799c41e2c36320325e461aae8b21b8d3
umaruch <130343724+starponycorp@users.noreply.github.com>
2023-09-22 21:06:57 +0500

    Create AccountEditDto.java

---
+++ src/main/java/com/starpony/prohojemba/dto/AccountEditDto.java
@@ -0,0 +1,60 @@
+package com.starpony.prohojemba.dto;
+
+
+public class AccountEditDto {
+    private String email;
+    private String username;
+    private String avatar;
+
+    private String password;
+    private String currentPassword;
+    private String verificationCode;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCurrentPassword() {
+        return currentPassword;
+    }
+
+    public void setCurrentPassword(String currentPassword) {
+        this.currentPassword = currentPassword;
+    }
+
+    public String getVerificationCode() {
+        return verificationCode;
+    }
+
+    public void setVerificationCode(String verificationCode) {
+        this.verificationCode = verificationCode;
+    }
+}
