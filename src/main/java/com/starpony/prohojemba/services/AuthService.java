3f79125cf94cc1103805deb76ab4bc0c32f88406
umaruch <130343724+starponycorp@users.noreply.github.com>
2023-09-25 20:30:36 +0500

    Отправка email с кодом

--- src/main/java/com/starpony/prohojemba/services/AuthService.java
+++ src/main/java/com/starpony/prohojemba/services/AuthService.java
@@ -6,6 +6,7 @@ import com.starpony.prohojemba.models.Account;
 import com.starpony.prohojemba.repositories.RefreshTokenRepository;
 import com.starpony.prohojemba.repositories.VerifyCodesRepository;
 import com.starpony.prohojemba.utils.JWTUtils;
+import com.starpony.prohojemba.utils.VerifyCodeEmailSender;
 import com.starpony.prohojemba.utils.VerifyCodeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,12 +18,14 @@ public class AuthService {
     private final RefreshTokenRepository refreshTokenRepository;
     private final VerifyCodesRepository verifyCodesRepository;
     private final JWTUtils jwtUtils;
+    private final VerifyCodeEmailSender verifyCodeEmailSender;
 
     @Autowired
-    public AuthService(RefreshTokenRepository refreshTokenRepository, VerifyCodesRepository verifyCodesRepository, JWTUtils jwtUtils) {
+    public AuthService(RefreshTokenRepository refreshTokenRepository, VerifyCodesRepository verifyCodesRepository, JWTUtils jwtUtils, VerifyCodeEmailSender verifyCodeEmailSender) {
         this.refreshTokenRepository = refreshTokenRepository;
         this.verifyCodesRepository = verifyCodesRepository;
         this.jwtUtils = jwtUtils;
+        this.verifyCodeEmailSender = verifyCodeEmailSender;
     }
 
 
@@ -41,7 +44,6 @@ public class AuthService {
         String code = VerifyCodeUtils.generate();
         verifyCodesRepository.create(verifyRequestDto.getVerifyType(), verifyRequestDto.getEmail(), code);
 
-        System.out.println(String.format("Generated verify code for %s: %s",
-                verifyRequestDto.getEmail(), code));
+        verifyCodeEmailSender.send(verifyRequestDto.getEmail(), verifyRequestDto.getVerifyType(), code);
     }
 }
