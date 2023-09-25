e8f9b7fcd10983c7ebd7935aec5a86b6b2c9fc3c
umaruch <130343724+starponycorp@users.noreply.github.com>
2023-09-25 17:47:12 +0500

    add spring mail dependency

---
+++ src/main/java/com/starpony/prohojemba/utils/VerifyCodeEmailSender.java
@@ -0,0 +1,12 @@
+package com.starpony.prohojemba.utils;
+
+import com.starpony.prohojemba.enums.VerifyType;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class VerifyCodeEmailSender {
+    public void send(String recipient, VerifyType verifyType, String code) {
+        
+    }
+}
